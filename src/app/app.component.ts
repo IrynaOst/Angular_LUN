0e68ff37bcc46b9f1c45ed5c510719f4ccd62c4d
IrynaOst <52861410+IrynaOst@users.noreply.github.com>
2019-10-30 13:52:32 +0200

    add input search, login and registration forms  and data houses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  inputedValue: string = '';
+  changeValue: string = '';
+  statusReg: boolean = false;
+  statusLog: boolean = false;
+  houses = [
+    {
+      city: 'Lviv',
+      street: 'Bandery',
+      square: 81,
+      price: 68000
+    },
+    {
+      city: 'Lviv',
+      street: 'Pasichna',
+      square: 103,
+      price: 96000
+    },
+    {
+      city: 'Rivne',
+      street: 'Hoholya',
+      square: 49,
+      price: 39000
+    },
+    {
+      city: 'Kyiv',
+      street: 'Franka',
+      square: 78,
+      price: 87000
+    },
+  ]
+
+  searchClick() {
+    this.changeValue = this.inputedValue;
+  }
+
+  onInput(ev) {
+    this.inputedValue = ev.target.value;
+  }
+  
+  register() {
+    this.statusReg = !this.statusReg;
+    this.statusLog = false;
+  }
+
+  login() {
+    this.statusLog = !this.statusLog;
+    this.statusReg = false;
+  }
+  
 }
